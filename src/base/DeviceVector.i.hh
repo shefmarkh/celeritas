826c0eadc27462dc0eb2b07719fba04d520bf00b
Mark Hodgkinson <shefmarkh@gmail.com>
2021-02-22 17:15:09 +0000

    Update DeviceVector to use local devices and queues, rather than global ones.

--- src/base/DeviceVector.i.hh
+++ src/base/DeviceVector.i.hh
@@ -10,13 +10,6 @@ using Dim = alpaka::dim::DimInt<1>;
 //Hard code for GPU right now for initial testing.
 //Eventually template class on Acc so we can switch?
 using Acc = alpaka::acc::AccGpuCudaRt<Dim, uint32_t>;
-//Make the first device (GPU) available
-static auto const device = alpaka::pltf::getDevByIdx<Acc>(0u);
-//Make the host CPU available
-static auto devHost = alpaka::pltf::getDevByIdx<alpaka::dev::DevCpu>(0u);
-//Make a queue we can use (later on this is going to have to be made accessible in some nice way)
-//for copying data to and from the device.
-static auto queue = alpaka::queue::Queue<Acc,alpaka::queue::Blocking>{device};
 
 namespace celeritas
 {
@@ -24,7 +17,7 @@ namespace celeritas
 //Default constructor (assumes sixe of zero)
 template<class T>
 DeviceVector<T>::DeviceVector() : bufferExtent_(static_cast<uint32_t>(0)), 
-allocatedMemory_(alpaka::mem::buf::alloc<T,uint32_t>(device,bufferExtent_)) {};
+allocatedMemory_(alpaka::mem::buf::alloc<T,uint32_t>(alpaka::pltf::getDevByIdx<Acc>(0u),bufferExtent_)) {};
 
 
 //---------------------------------------------------------------------------//
@@ -33,7 +26,7 @@ allocatedMemory_(alpaka::mem::buf::alloc<T,uint32_t>(device,bufferExtent_)) {};
  */
 template<class T>
 DeviceVector<T>::DeviceVector(size_type count)
-    :  bufferExtent_{static_cast<uint32_t>(count)},  allocatedMemory_(alpaka::mem::buf::alloc<T,uint32_t>(device,bufferExtent_))    
+    :  bufferExtent_{static_cast<uint32_t>(count)},  allocatedMemory_(alpaka::mem::buf::alloc<T,uint32_t>(alpaka::pltf::getDevByIdx<Acc>(0u),bufferExtent_))    
 {
 }
 
@@ -58,10 +51,14 @@ void DeviceVector<T>::copy_to_device(constSpan_t data)
 {
     REQUIRE(data.size() == this->size());
     //Create a local memory buffer for the host data
-    auto hostMemoryBuffer(alpaka::mem::buf::alloc<T,uint32_t>(devHost,bufferExtent_));
+    auto hostMemoryBuffer(alpaka::mem::buf::alloc<T,uint32_t>(alpaka::pltf::getDevByIdx<alpaka::dev::DevCpu>(0u),bufferExtent_));
     //Now make the pointers in that buffer point at the data in the Span (data) passed into this function
-    for (uint32_t counter = 0; counter < this->size(); counter++) alpaka::mem::view::getPtrNative(hostMemoryBuffer)[counter] = data[counter];
-    //Finally copy the host data into the device memory allocatedMemory_
+    for (uint32_t counter = 0; counter < this->size(); counter++) alpaka::mem::view::getPtrNative(hostMemoryBuffer)[counter] = data[counter];   
+    //Make the first device (GPU) available
+    auto device = alpaka::pltf::getDevByIdx<Acc>(0u);
+    //Make a queue
+    auto queue = alpaka::queue::Queue<Acc,alpaka::queue::Blocking>{device};
+     //Finally copy the host data into the device memory allocatedMemory_
     alpaka::mem::view::copy(queue,allocatedMemory_, hostMemoryBuffer,bufferExtent_);    
 }
 
@@ -74,7 +71,11 @@ void DeviceVector<T>::copy_to_host(Span_t data) const
 {    
     REQUIRE(data.size() == this->size());
     //Create a local memory buffer for the host data
-    auto hostMemoryBuffer(alpaka::mem::buf::alloc<T,uint32_t>(devHost,bufferExtent_));
+    auto hostMemoryBuffer(alpaka::mem::buf::alloc<T,uint32_t>(alpaka::pltf::getDevByIdx<alpaka::dev::DevCpu>(0u),bufferExtent_));    
+    //Make the first device (GPU) available
+    auto device = alpaka::pltf::getDevByIdx<Acc>(0u);
+    //Make a queue
+    auto queue = alpaka::queue::Queue<Acc,alpaka::queue::Blocking>{device};
     //Now copy the device data into that buffer
     alpaka::mem::view::copy(queue,hostMemoryBuffer,allocatedMemory_,bufferExtent_);    
     //Now copy the data pointed at by the alpaka host memory buffer into the Span (data)
